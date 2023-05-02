6a9a9aa08e0b68bcfeb1e5898909fc23dfb887b3
Martijn Webbio <martijn@webbio.nl>
2023-05-02 17:21:25 +0200

    first commit

---
+++ src/components/post-detail/post-detail.tsx
@@ -0,0 +1,16 @@
+const PostDetail = (): JSX.Element => {
+  return (
+    <div className="max-w-2xl mx-auto w-full mt-10">
+      <h1 className="text-5xl font-extrabold mb-4">His mother had always taught him</h1>
+      <div>author</div>
+      <img src="assets/posts/1.jpg" />
+      <p className="text-xl mt-4">
+        His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this
+        motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity
+        of the group of people he was talking to made him change his mind.
+      </p>
+    </div>
+  );
+};
+
+export { PostDetail };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="nl">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@300..900&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
